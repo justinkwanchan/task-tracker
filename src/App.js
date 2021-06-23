950dfcd6b51a7c4bcbf27361e7219a106807200a
Justin <justinkwanchan@gmail.com>
2021-06-23 17:41:32 -0400

    Implement deleting task from JSON server

--- src/App.js
+++ src/App.js
@@ -45,7 +45,11 @@ function App() {
   };
 
   // Delete Task
-  const deleteTask = id => {
+  const deleteTask = async id => {
+    fetch(`http://localhost:5000/tasks/${id}`, {
+      method: 'DELETE',
+    });
+
     setTasks(tasks.filter(task => task.id !== id));
   };
 
